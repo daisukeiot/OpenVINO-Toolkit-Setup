3b2bc658982fbfd34a67c988cb24b4140d9af3aa
Daisuke Nakahara <daisuken@msn.com>
2020-03-12 16:38:19 -0700

    Add setupvars.sh

--- Container/dockerfile/benchmark.sh
+++ Container/dockerfile/benchmark.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+source /opt/intel/openvino/bin/setupvars.sh
 cd /home/${USER}/inference_engine_samples_build/intel64/Release
 echo ''
 echo '###############################################################################'
 echo "Running on ${TARGET}"
 echo '###############################################################################'
 
-./benchmark_app -d ${TARGET} -i ${INSTALLDIR}/deployment_tools/demo/car.png -m /home/${USER}/ir/public/alexnet/FP16/alexnet/FP16/alexnet.xml
+./benchmark_app -d ${TARGET} -i ${INSTALLDIR}/deployment_tools/demo/car.png -m /home/${USER}/openvino_models/ir/public/alexnet/FP16/alexnet.xml
