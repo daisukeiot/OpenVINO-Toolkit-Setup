1f4d8a008e0a0d464f040a672ac690b2001d19b9
Daisuke Nakahara <daisuken@msn.com>
2020-03-12 14:41:38 -0700

    Update verify-container.sh

--- Container/verify-container.sh
+++ Container/verify-container.sh
@@ -16,6 +16,6 @@ OPENVINO_VER=2019.3.376
 #
 # Run Verification Script
 #
-docker run --rm -it -e "TARGET=CPU" ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify
-docker run --rm -it -e "TARGET=GPU" --device /dev/dri ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify
+docker run --rm -it -e "TARGET=CPU" ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify /home/openvino/verify.sh
+docker run --rm -it -e "TARGET=GPU" --device /dev/dri ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify /home/openvino/verify.sh
 docker run --rm -it -e "TARGET=MYRIAD" --privileged -v /dev:/dev --network=host ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify
