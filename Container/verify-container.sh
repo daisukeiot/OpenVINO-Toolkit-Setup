46f3060bca7230618ef08a6583a1e349462b6590
Daisuke Nakahara <daisuken@msn.com>
2020-03-12 14:32:42 -0700

    Update verify-container.sh

--- Container/verify-container.sh
+++ Container/verify-container.sh
@@ -1,18 +1,21 @@
+if [ $# -eq 0 ]
+  then
+    echo "======================================="
+    echo "Please specify Ubuntu Version"
+    echo "18.04 or 16.04"
+    echo "======================================="
+    exit
+fi
 #
 # Build Container with verification script
 # Built container but do not push to registry
 #
+UBUNTU_VER=$1
 MY_REGISTRY=daisukeiot
 OPENVINO_VER=2019.3.376
 #
 # Run Verification Script
 #
-UBUNTU_VER=16.04
 docker run --rm -it -e "TARGET=CPU" ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify
 docker run --rm -it -e "TARGET=GPU" --device /dev/dri ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify
-docker run --rm -it -e "TARGET=MYRIAD" --privileged -v /dev:/dev ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify
-
-UBUNTU_VER=18.04
-docker run --rm -it -e "TARGET=CPU" ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify
-docker run --rm -it -e "TARGET=GPU" --device /dev/dri ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify
-docker run --rm -it -e "TARGET=MYRIAD" --privileged -v /dev:/dev ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify
+docker run --rm -it -e "TARGET=MYRIAD" --privileged -v /dev:/dev --network=host ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify
