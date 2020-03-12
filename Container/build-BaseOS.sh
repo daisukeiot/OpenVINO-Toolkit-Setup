a9d62b68ceaf0dc90ed156aa03f60c718c5ae4b3
Daisuke Nakahara <daisuken@msn.com>
2020-03-12 14:43:36 -0700

    Update build-BaseOS.sh

--- Container/build-BaseOS.sh
+++ Container/build-BaseOS.sh
@@ -14,7 +14,7 @@ MY_REGISTRY=daisukeiot
 # Build Ubuntu Base Image
 #
 docker build --squash --rm -f ./dockerfile/Dockerfile-Ubuntu --build-arg UBUNTU_VER=${UBUNTU_VER} -t ${MY_REGISTRY}/openvino-toolkit:baseos-ubuntu_${UBUNTU_VER} .
-# docker push ${MY_REGISTRY}/openvino-toolkit:baseos-ubuntu_${UBUNTU_VER}
+docker push ${MY_REGISTRY}/openvino-toolkit:baseos-ubuntu_${UBUNTU_VER}
 
 echo ''
 echo '###############################################################################'
