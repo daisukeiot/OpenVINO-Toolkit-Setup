7767fa5368eae6e9d432e7e52740ce9468a3482c
Daisuke Nakahara <daisuken@msn.com>
2020-03-12 16:50:39 -0700

    Add Benchmark Tool to script

--- Container/verify-container.sh
+++ Container/verify-container.sh
@@ -16,9 +16,17 @@ fi
 UBUNTU_VER=$1
 MY_REGISTRY=$2
 OPENVINO_VER=2019.3.376
+
 #
-# Run Verification Script
+# Image Classification Sample
 #
 docker run --rm -it -e "TARGET=CPU" ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify /home/openvino/verify.sh
 docker run --rm -it -e "TARGET=GPU" --device /dev/dri ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify /home/openvino/verify.sh
 docker run --rm -it -e "TARGET=MYRIAD" --privileged -v /dev:/dev --network=host ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify /home/openvino/verify.sh
+
+#
+# Benchmark Tool
+#
+docker run --rm -it -e "TARGET=CPU" ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify /home/openvino/benchmark.sh
+docker run --rm -it -e "TARGET=GPU" --device /dev/dri ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify /home/openvino/benchmark.sh
+docker run --rm -it -e "TARGET=MYRIAD" --privileged -v /dev:/dev --network=host ${MY_REGISTRY}/openvino-toolkit:${UBUNTU_VER}_${OPENVINO_VER}_verify /home/openvino/benchmark.sh
