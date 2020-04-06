707d0dd1ec0e9a991c7c21b38e4d9a0212b08eb1
Daisuke Nakahara <daisuken@msn.com>
2020-04-06 15:03:50 -0700

    Update build-Verify-Container.sh

--- Container/build-Verify-Container.sh
+++ Container/build-Verify-Container.sh
@@ -11,8 +11,11 @@ fi
 # Build Container with verification script
 # Built container but do not push to registry
 #
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 TAG=$1
 TARGET_TAG=$1_verify
-docker build --squash --rm -f ./Common/Classification-Demo_Benchmark/Dockerfile -t ${TARGET_TAG} --build-arg OPENVINO_IMAGE=${TAG} ./Common/Classification-Demo_Benchmark
-
-./run-Verify-Container.sh ${TARGET_TAG} CPU
+docker build --squash --rm -f \
+    ${SCRIPT_DIR}/Common/Classification-Demo_Benchmark/Dockerfile \
+    -t ${TARGET_TAG} \
+    --build-arg OPENVINO_IMAGE=${TAG} \
+    ${SCRIPT_DIR}/Common/Classification-Demo_Benchmark
