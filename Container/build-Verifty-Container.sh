98c5f0402c2f6293921867c146398663217fc070
Daisuke Nakahara <daisuken@msn.com>
2020-04-06 11:07:45 -0700

    Update build-Verifty-Container.sh

--- Container/build-Verifty-Container.sh
+++ Container/build-Verifty-Container.sh
@@ -12,5 +12,5 @@ fi
 # Built container but do not push to registry
 #
 TAG=$1
-
-docker build --squash --rm -f ./Common/Classification-Demo_Benchmark/Dockerfile -t ${TAG} --build-arg OPENVINO_IMAGE=${TAG} ./Common/Classification-Demo_Benchmark
+TARGET_TAG=$1_verify
+docker build --squash --rm -f ./Common/Classification-Demo_Benchmark/Dockerfile -t ${TARGET_TAG} --build-arg OPENVINO_IMAGE=${TAG} ./Common/Classification-Demo_Benchmark
