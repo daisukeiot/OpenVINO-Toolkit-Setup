775ed052cfbbba8a64ce7aba0439f6d6fe5c9cbc
Daisuke Nakahara <daisuken@msn.com>
2020-04-06 11:09:39 -0700

    Update build-Verifty-Container.sh

--- Container/build-Verifty-Container.sh
+++ Container/build-Verifty-Container.sh
@@ -14,3 +14,6 @@ fi
 TAG=$1
 TARGET_TAG=$1_verify
 docker build --squash --rm -f ./Common/Classification-Demo_Benchmark/Dockerfile -t ${TARGET_TAG} --build-arg OPENVINO_IMAGE=${TAG} ./Common/Classification-Demo_Benchmark
+
+docker run --rm -it ${TARGET_TAG} /bin/bash /home/openvino/verify.sh
+docker run --rm -it ${TARGET_TAG} /bin/bash /home/openvino/benchmark.sh
