ad8c548ef99729f0d6e95c0a03a54cbff7f118fe
Daisuke Nakahara <daisuken@msn.com>
2020-04-06 10:44:15 -0700

    rename

---
+++ Container/build-Verifty-Container.sh
@@ -0,0 +1,16 @@
+if [ $# -ne 1 ]
+  then
+    echo "======================================="
+    echo "Please specify Container"
+    echo "  Example ./verify-container.sh myregistry/container:tag"
+    echo "======================================="
+    exit
+fi
+
+#
+# Build Container with verification script
+# Built container but do not push to registry
+#
+TAG=$1
+
+docker build --squash --rm -f ./Common/Classification-Demo_Benchmark/Dockerfile.Benchmark -t ${TAG} --build-arg OPENVINO_IMAGE=${TAG} .
