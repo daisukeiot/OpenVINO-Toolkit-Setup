efeefe7899be8a8097bdff950791987444308399
Daisuke Nakahara <daisuken@msn.com>
2020-04-06 18:09:17 -0700

    Add script for Hello Query Devices script

---
+++ Container/Common/Classification-Demo_Benchmark/hello_device.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]
+  then
+    TARGET=$1
+fi
+
+source /opt/intel/openvino/bin/setupvars.sh
+cd /home/${USER}/inference_engine_samples_build/intel64/Release
+echo ''
+echo '    __  __     ____         ____            _         '
+echo '   / / / /__  / / /___     / __ \___ _   __(_)_______ '
+echo '  / /_/ / _ \/ / / __ \   / / / / _ \ | / / / ___/ _ \'
+echo ' / __  /  __/ / / /_/ /  / /_/ /  __/ |/ / / /__/  __/'
+echo '/_/ /_/\___/_/_/\____/  /_____/\___/|___/_/\___/\___/ '
+echo ''
+echo '###################################################################################################'
+echo "Running on ${TARGET}"
+echo '###################################################################################################'
+
+cd ${INSTALLDIR}/inference_engine/samples/python_samples/hello_query_device
+python3 ./hello_query_device.py
