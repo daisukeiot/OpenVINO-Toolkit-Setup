be83cbd20958167c716c80719ef3f72b2f0f1013
Daisuke Nakahara <daisuken@msn.com>
2020-04-08 19:31:40 -0700

    Update main.py

--- App/ObjectDetection/Python/main.py
+++ App/ObjectDetection/Python/main.py
@@ -16,61 +16,61 @@ async def main():
 
     tasks = []
 
-    # try:
-    #
-    # Default to USB Webcam
-    #
-    async with VideoProcessor(devicePath = '/dev/video0') as videoProcessor:
-
-        loop = asyncio.get_event_loop()
-
+    try:
         #
-        # Initialize Tornado Web Server
+        # Default to USB Webcam
         #
-        webServer = WebServer(videoProcessor = videoProcessor, port = 80)
+        async with VideoProcessor(devicePath = '/dev/video0') as videoProcessor:
 
-        if (sys.version_info >= (3, 7)):
-            loop = asyncio.get_running_loop()
-            loop.set_debug(False)
-        else:
             loop = asyncio.get_event_loop()
-            loop.set_debug(False)
 
-        #
-        # Create a Thread Pool Executor to run Video Stream and Video Processor
-        #
-        videoExecutor = ThreadPoolExecutor(max_workers=3, thread_name_prefix='videoThread')
+            #
+            # Initialize Tornado Web Server
+            #
+            webServer = WebServer(videoProcessor = videoProcessor, port = 80)
 
-        #
-        # Start Capturing from Video
-        #
-        streamLoop = asyncio.new_event_loop()
-        videoStreamTask = streamLoop.run_in_executor(videoExecutor, videoProcessor.videoStream.capture_video_frame)
-        tasks.append(videoStreamTask)
-        #
-        # Start Processing video frames
-        #
-        videoLoop = asyncio.new_event_loop()
-        videoProcessTask = videoLoop.run_in_executor(videoExecutor, videoProcessor.process_video_frame)
-        tasks.append(videoProcessTask)
-        #
-        # Start Tornado Web Server
-        #
-        webServer.startWebServer()
+            if (sys.version_info >= (3, 7)):
+                loop = asyncio.get_running_loop()
+                loop.set_debug(False)
+            else:
+                loop = asyncio.get_event_loop()
+                loop.set_debug(False)
 
-        #
-        # keyboard input
-        #
-        inputTask = loop.run_in_executor(None, stdin_listener)
-        await inputTask
+            #
+            # Create a Thread Pool Executor to run Video Stream and Video Processor
+            #
+            videoExecutor = ThreadPoolExecutor(max_workers=3, thread_name_prefix='videoThread')
 
-        await asyncio.wait(tasks)
+            #
+            # Start Capturing from Video
+            #
+            streamLoop = asyncio.new_event_loop()
+            videoStreamTask = streamLoop.run_in_executor(videoExecutor, videoProcessor.videoStream.capture_video_frame)
+            tasks.append(videoStreamTask)
+            #
+            # Start Processing video frames
+            #
+            videoLoop = asyncio.new_event_loop()
+            videoProcessTask = videoLoop.run_in_executor(videoExecutor, videoProcessor.process_video_frame)
+            tasks.append(videoProcessTask)
+            #
+            # Start Tornado Web Server
+            #
+            webServer.startWebServer()
 
-    # except CancelledError:
-    #     logging.info('-- {0}() - Cancelled'.format(sys._getframe().f_code.co_name))
+            #
+            # keyboard input
+            #
+            inputTask = loop.run_in_executor(None, stdin_listener)
+            await inputTask
 
-    # except Exception as ex:
-    #     logging.info('<< {0}() ****  Exception {1} ****'.format(sys._getframe().f_code.co_name, ex))
+            await asyncio.wait(tasks)
+
+    except CancelledError:
+        logging.info('-- {0}() - Cancelled'.format(sys._getframe().f_code.co_name))
+
+    except Exception as ex:
+        logging.info('<< {0}() ****  Exception {1} ****'.format(sys._getframe().f_code.co_name, ex))
 
 def stdin_listener():
     while True:
