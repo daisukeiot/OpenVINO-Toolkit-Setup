72ef08083693cf8bde2a4f29a4ae3df8998d8d0b
Daisuke Nakahara <daisuken@msn.com>
2020-04-23 17:11:49 -0700

    Set Executable attribute

--- App/ObjectDetection/Python/setup.sh
+++ App/ObjectDetection/Python/setup.sh
@@ -1,5 +1,3 @@
-cd ~ && \
-git clone https://github.com/daisukeiot/OpenVINO-Toolkit-Setup.git && \
-cd OpenVINO-Toolkit-Setup/App/ObjectDetection/Python && \
+cd ~/OpenVINO-Toolkit-Setup/App/ObjectDetection/Python && \
 git clone https://github.com/opencv/open_model_zoo.git && \
 pip3 install -r ./requirements.txt
