807cd4bd08f42851105cc374875adaf1af36a2cd
Daisuke Nakahara <daisuken@msn.com>
2020-04-27 03:55:10 -0700

    Update sample path

--- Container/Common/Classification-Demo_Benchmark/benchmark.sh
+++ Container/Common/Classification-Demo_Benchmark/benchmark.sh
@@ -21,10 +21,14 @@ echo "Running on ${TARGET}"
 echo '###############################################################################'
 
 cd ${INTEL_OPENVINO_DIR}/deployment_tools/tools/model_downloader
+python3 -mpip install --user -r ./requirements.in
 python3 ./downloader.py --name alexnet --output /home/$USER/openvino_models/
-cd ${INTEL_OPENVINO_DIR}/deployment_tools/model_optimizer/
+cd ${INTEL_OPENVINO_DIR}/deployment_tools/model_optimizer/install_prerequisites
+./install_prerequisites.sh
 python3 ./mo.py --data_type=FP16 --input_model /home/$USER/openvino_models/public/alexnet/alexnet.caffemodel -o /home/$USER/openvino_models/ir/public/alexnet/FP16/
-python3 ./mo.py --data_type=FP32 --input_model /home/$USER/openvino_models/public/alexnet/alexnet.caffemodel -o /home/$USER/openvino_models/ir/public/alexnet/FP32/ 
+python3 ./mo.py --data_type=FP32 --input_model /home/$USER/openvino_models/public/alexnet/alexnet.caffemodel -o /home/$USER/openvino_models/ir/public/alexnet/FP32/
+cd ${INTEL_OPENVINO_DIR}/rence_engine/samples/cpp
+./build_samples.sh
 cd /home/$USER/inference_engine_samples_build/benchmark_app
 make
 ./benchmark_app -d ${TARGET} -i ${INSTALLDIR}/deployment_tools/demo/car.png -m /home/${USER}/openvino_models/ir/public/alexnet/FP16/alexnet.xml
