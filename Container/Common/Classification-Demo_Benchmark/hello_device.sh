a5d5b5915734d4a964897f87dc88c52c7d250c08
Daisuke Nakahara <daisuken@msn.com>
2020-04-27 03:38:23 -0700

    Update path for python

--- Container/Common/Classification-Demo_Benchmark/hello_device.sh
+++ Container/Common/Classification-Demo_Benchmark/hello_device.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [ $# -eq 1 ]
-  then
-    TARGET=$1
-fi
-
 source /opt/intel/openvino/bin/setupvars.sh
-cd /home/${USER}/inference_engine_samples_build/intel64/Release
 echo ''
 echo '    __  __     ____         ____            _         '
 echo '   / / / /__  / / /___     / __ \___ _   __(_)_______ '
@@ -15,9 +9,7 @@ echo ' / __  /  __/ / / /_/ /  / /_/ /  __/ |/ / / /__/  __/'
 echo '/_/ /_/\___/_/_/\____/  /_____/\___/|___/_/\___/\___/ '
 echo ''
 echo '###################################################################################################'
-echo "Running on ${TARGET}"
-echo '###################################################################################################'
 
-cd ${INSTALLDIR}/inference_engine/samples/python_samples
+cd ${INSTALLDIR}/inference_engine/samples/python
 pip3 install -r ./requirements.txt
 python3 ./hello_query_device/hello_query_device.py
