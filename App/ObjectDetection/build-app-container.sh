680a6b13848bf5a6cb8601fe72d5bfa66e7bcf15
Daisuke Nakahara <daisuken@msn.com>
2020-04-28 17:58:35 -0700

    Update build-app-container.sh

--- App/ObjectDetection/build-app-container.sh
+++ App/ObjectDetection/build-app-container.sh
@@ -1,20 +1,24 @@
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
   then
     echo "======================================="
-    echo "Please specify Container"
-    echo "  Example ./build-python-app-container.sh myregistry/container:tag"
+    echo "Please specify base image tag and reistry"
+    echo "  Image Tag      : Image Tag for a container with OpenVINO toolkit"
+    echo "  Registry       : Your registry"
+    echo "  Example ./build-app-container.sh openvino-container:ubuntu18.04_openvino2020.2.120_cp3.7 myregistry"
     echo "======================================="
     exit
 fi
-
 #
 # Build Container with verification script
 # Built container but do not push to registry
 #
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-BASE_TAG=$1
-TARGET_TAG=$1_app_0.1
+OPENVINO_VER=2020.2.120
+MY_REGISTRY=$2
+BASE_TAG=${MY_REGISTRY}/$1
+TARGET_TAG=${BASE_TAG}_app
+
 docker build --squash --rm \
     -f ./Dockerfile \
     --build-arg BASE_TAG=${BASE_TAG} \
