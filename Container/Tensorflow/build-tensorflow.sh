264ee2a2cb0c3b0c15b5c55ede1eff834158484b
Daisuke Nakahara <daisuken@msn.com>
2020-04-29 17:33:34 -0700

    force deleting dangling images

--- Container/Tensorflow/build-tensorflow.sh
+++ Container/Tensorflow/build-tensorflow.sh
@@ -121,4 +121,4 @@ bazel-bin/tensorflow/tools/pip_package/build_pip_package "/wheels/$SUBFOLDER_NAM
 # Fix wheel folder permissions
 chmod -R 777 /wheels/
 
-docker rmi $(docker images -f "dangling=true" -q)
+docker rmi -f $(docker images -f "dangling=true" -q)
