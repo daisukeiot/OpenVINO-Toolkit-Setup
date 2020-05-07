7dd420ac7d3cc69cec911381ac27e719a15f0fab
Daisuke Nakahara <daisuken@msn.com>
2020-05-06 18:06:08 -0700

    Update run_imageclassification_Windows.cmd

--- Container/run_imageclassification_Windows.cmd
+++ Container/run_imageclassification_Windows.cmd
@@ -1,10 +1,10 @@
 if "%1" equ "" goto :HELP
 
-#
-# Build Container with verification script
-# Built container but do not push to registry
-#
-TARGET_TAG=$1
+REM
+REM Build Container with verification script
+REM Built container but do not push to registry
+REM
+SET TARGET_TAG=%1
 
 echo ''
 echo '    ____                              ________                _ _____            __  _           '
@@ -14,10 +14,10 @@ echo ' _/ // / / / / / /_/ / /_/ /  __/  / /___/ / /_/ (__  |__  ) / __/ / /__/ 
 echo '/___/_/ /_/ /_/\__,_/\__, /\___/   \____/_/\__,_/____/____/_/_/ /_/\___/\__,_/\__/_/\____/_/ /_/ '
 echo '                    /____/                                                                       '
 echo '###################################################################################################'
-echo "Running on ${TARGET_DEV}"
+echo "Running on %TARGET_DEV%"
 echo '###################################################################################################'
 
-docker run --rm --isolation hyperv ${TARGET_TAG} cmd.exe /c "cd %INTEL_OPENVINO_DIR%\deployment_tools\demo && demo_squeezenet_download_convert_run.bat"
+docker run --rm --isolation hyperv %TARGET_TAG% cmd.exe /c "cd %INTEL_OPENVINO_DIR%\deployment_tools\demo && demo_squeezenet_download_convert_run.bat"
 
 goto :EOF
 
