e64677a0b39fed95fe8d809216f9f5d7daa04c66
Daisuke Nakahara <daisuken@msn.com>
2020-05-07 03:50:30 -0700

    Update build-Ubuntu-UP2.sh

--- Container/build-Ubuntu-UP2.sh
+++ Container/build-Ubuntu-UP2.sh
@@ -1,35 +1,60 @@
-if [ $# -ne 4 ]
+if [ $# -ne 3 ]
   then
     echo "======================================="
     echo "Please specify Ubuntu Version and reistry"
-    echo "  Ubuntu Version : 18.04 or 16.04"
     echo "  Registry       : Your registry"
+    echo "  Ubuntu Version : 18.04 or 16.04"
     echo "  Python         : Python Version"
-    echo "  Tensorflow     : Tensorflow Version"
-    echo "  Example ./build-Ubuntu-UP2.sh 18.04 myregistry 3.7 r1.14"
+    echo ""
+    echo "  Example : ${0##*/} myregistry 18.04 3.7"
     echo "======================================="
     exit
 fi
+[ "$DEBUG" ] && set -x
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+clear
 
-OS_VERSION=$1
-MY_REGISTRY=$2
+MY_REGISTRY=$1
+OS_VERSION=$2
 PYTHON_VERSION=$3
-TF_VERSION=$4
+TF_VERSION=1.15
+OPENVINO_VER=2020.2.120
 
-TAG_OS=${MY_REGISTRY}/openvino-container:baseos-ubuntu_${OS_VERSION}_cp${PYTHON_VERSION}
+cd ./Ubuntu
 
-if docker inspect --type=image $TAG_OS > /dev/null 2>&1; then
-    echo "${TAG_OS} exists"
-  else
-    ./Ubuntu/build-BaseOS.sh ${OS_VERSION} ${MY_REGISTRY}
+TAG=ubuntu_${OS_VERSION}_cp${PYTHON_VERSION}
+
+if ! docker inspect --type=image ${MY_REGISTRY}/openvino-container:$TAG > /dev/null 2>&1; then
+  ./build-BaseOS.sh ${MY_REGISTRY} ${OS_VERSION} ${PYTHON_VERSION}
 fi
 
-TAG_TF=${MY_REGISTRY}/openvino-container:tf_${TF_VERSION}_cp${PYTHON_VERSION}
-
-if docker inspect --type=image $TAG_TF > /dev/null 2>&1; then
-    echo "${TAG_TF} exists"
-  else
-    ./Tensorflow/build-Tensorflow-NoAVX.sh ${OS_VERSION} ${MY_REGISTRY} ${PYTHON_VERSION} ${TF_VERSION}
+# Install Tensorflow
+./build-Tensorflow.sh ${MY_REGISTRY} ${OS_VERSION} ${PYTHON_VERSION} ${TF_VERSION}
+TAG=ubuntu_${OS_VERSION}_cp${PYTHON_VERSION}_tf${TF_VERSION}
+if ! docker inspect --type=image ${MY_REGISTRY}/openvino-container:$TAG > /dev/null 2>&1; then
+    echo "Failed to create image"
+    exit
 fi
 
-./Ubuntu/build-OpenVINO-Toolkit-UP2.sh ${OS_VERSION} ${MY_REGISTRY} ${PYTHON_VERSION} ${TF_VERSION}
+# Install OpenVINO
+./build-OpenVINO-Toolkit.sh ${MY_REGISTRY} ${TAG}
+TAG=${TAG}_ov${OPENVINO_VER}
+if ! docker inspect --type=image ${MY_REGISTRY}/openvino-container:$TAG > /dev/null 2>&1; then
+    echo "Failed to create image"
+    exit
+fi
+
+./build-Demo.sh ${MY_REGISTRY}/openvino-container:${TAG} 3.6
+TAG=${TAG}_demo_3.6
+if ! docker inspect --type=image ${MY_REGISTRY}/openvino-container:$TAG > /dev/null 2>&1; then
+    echo "Failed to create image"
+    exit
+fi
+
+./build-Demo.sh ${MY_REGISTRY}/openvino-container:${TAG} 3.7
+TAG=${TAG}_demo_3.7
+if ! docker inspect --type=image ${MY_REGISTRY}/openvino-container:$TAG > /dev/null 2>&1; then
+    echo "Failed to create image"
+    exit
+fi
+# docker rmi -f $(docker images -f "dangling=true" -q)
