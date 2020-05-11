cee5ee5763644ec053b6b2ff21c93fe3f4e7b3d0
Daisuke Nakahara <daisuken@msn.com>
2020-05-10 18:59:47 -0700

    Update setup3.sh

--- Setup/UP2/setup3.sh
+++ Setup/UP2/setup3.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cd ~
-sudo apt-get install -y curl 
+sudo apt-get install -y curl
+
+#
+# Install Moby so we can copy Tensorflow withtou AVX from container image
+#
 curl https://packages.microsoft.com/config/ubuntu/18.04/multiarch/prod.list > ./microsoft-prod.list 
 sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/ 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg 
@@ -14,15 +18,23 @@ sudo usermod -aG docker $USER
 TAG=daisukeiot/openvino-container:tf_r1.15_data 
 sudo docker run --name tensorflow ${TAG} /bin/true 
 sudo docker cp tensorflow:/tensorflow-1.15.2-cp37-cp37m-linux_x86_64.whl ./ 
+#
+# Clean up
+#
 sudo docker rm tensorflow 
 sudo docker rmi -f ${TAG} 
 sudo apt-get remove -y --purge moby-cli
 sudo apt-get remove -y --purge moby-engine
-sudo apt-get install -y curl python3-pip python3.7 python3.7-dev 
+
+#
+# Install Python 3.7 so we can install numpy v 1.16 before tensorflow
+#
+sudo apt-get install -y curl python3.7 python3.7-dev 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py 
+sudo python3.6 get-pip.py 
 sudo python3.7 get-pip.py 
 rm get-pip.py 
-cd /usr/lib/python3/dist-packages 
+cd /usr/lib/python3/dist-packages
 sudo ln -s apt_pkg.cpython-{36m,37m}-x86_64-linux-gnu.so 
 cd ~ 
 sudo python3.7 -m pip install 'numpy==1.16' --force-reinstall 
