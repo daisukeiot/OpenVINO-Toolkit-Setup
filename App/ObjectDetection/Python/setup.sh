208128ae0c9c48eaadd84936f948f569deadd502
Daisuke Nakahara <daisuken@msn.com>
2020-05-11 14:56:42 -0700

    Update setup.sh

--- App/ObjectDetection/Python/setup.sh
+++ App/ObjectDetection/Python/setup.sh
@@ -4,7 +4,9 @@ if [ ! -d "open_model_zoo" ]; then
   git clone https://github.com/opencv/open_model_zoo.git
 fi
 
-cd ${INTEL_OPENVINO_DIR}/deployment_tools/model_optimizer
-sudo -H python3.7 -m pip install -r requirements.txt
-cd ${SCRIPT_DIR}
-sudo -H python3.7 -m pip install -r ./requirements.txt
+if [ -d ${INTEL_OPENVINO_DIR}/deployment_tools/model_optimizer ]; then
+  cd ${INTEL_OPENVINO_DIR}/deployment_tools/model_optimizer
+  sudo -H python3.7 -m pip install -r requirements.txt
+  cd ${SCRIPT_DIR}
+  sudo -H python3.7 -m pip install -r ./requirements.txt
+fi
