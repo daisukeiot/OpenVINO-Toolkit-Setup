7c9f644ada626748d748ffd5e17d68f600d7a626
Daisuke Nakahara <daisuken@msn.com>
2020-05-12 00:56:11 -0700

    Update verification.sh

--- Setup/Raspbian/verification.sh
+++ Setup/Raspbian/verification.sh
@@ -1,5 +1,5 @@
-SAMPLE_DIR=/home/${whoami}/build
-
+SAMPLE_DIR=/home/$(whoami)/build
+echo ${SAMPLE_DIR}
 cd ~/
 
 if [ ! -d "${SAMPLE_DIR}" ]; then
@@ -10,15 +10,26 @@ cd ${SAMPLE_DIR}
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-march=armv7-a" /opt/intel/openvino/deployment_tools/inference_engine/samples/cpp
 make -j2 object_detection_sample_ssd
 
-if [ ! -f "face-detection-0100.xml" ]; then
-    wget --no-check-certificate https://download.01.org/opencv/2020/openvinotoolkit/2020.2/open_model_zoo/models_bin/3/face-detection-0100/FP16/face-detection-0100.xml
+if [ -f "face-detection-adas-0001.xml" ]; then
+    rm face-detection-adas-0001.xml
 fi
 
-if [ ! -f "face-detection-0100.bin" ]; then
-    wget --no-check-certificate https://download.01.org/opencv/2020/openvinotoolkit/2020.2/open_model_zoo/models_bin/3/face-detection-0100/FP16/face-detection-0100.bin
+if [ -f "face-detection-adas-0001.bin" ]; then
+    rm face-detection-adas-0001.bin
 fi
 
-if [ ! -f "face.jpg" ]; then
-    wget --no-check-certificate https://github.com/intel-iot-devkit/inference-tutorials-generic/raw/openvino_toolkit_2019_r1_0/face_detection_tutorial/data/face.jpg
+if [ -f "face.jpg" ]; then
+    rm face.jpg
 fi
-./armv7l/Release/object_detection_sample_ssd -m ${SAMPLE_DIR}/face-detection-adas-0001.xml -d MYRIAD -i ${SAMPLE_DIR}/face.jpg
+
+wget --no-check-certificate https://github.com/intel-iot-devkit/inference-tutorials-generic/raw/openvino_toolkit_2019_r1_0/face_detection_tutorial/data/face.jpg
+
+# IRv10 does not work https://github.com/openvinotoolkit/openvino/issues/411
+# wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R4/20191230_170000_models_bin/face-detection-retail-0004/FP16/face-detection-retail-0004.bin
+# wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R4/20191230_170000_models_bin/face-detection-retail-0004/FP16/face-detection-retail-0004.xml
+
+# use IRv7
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R4/20191121_190000_models_bin/face-detection-retail-0004/FP16/face-detection-retail-0004.bin
+wget --no-check-certificate https://download.01.org/opencv/2019/open_model_zoo/R4/20191121_190000_models_bin/face-detection-retail-0004/FP16/face-detection-retail-0004.xml
+
+./armv7l/Release/object_detection_sample_ssd -m ${SAMPLE_DIR}/face-detection-retail-0004.xml -d MYRIAD -i ${SAMPLE_DIR}/face.jpg
