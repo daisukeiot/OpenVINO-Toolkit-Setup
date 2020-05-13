14c2a6e37bee95fccab1740491eeef3cc04ae979
Daisuke Nakahara <daisuken@msn.com>
2020-05-13 08:07:48 -0700

    Update setup.sh

--- Setup/Raspbian/setup.sh
+++ Setup/Raspbian/setup.sh
@@ -29,11 +29,11 @@ sudo apt-get install -y \
     libatlas-base-dev \
     gfortran \
     python3-pip \
-    curl 
-    # libgstreamer1.0-0 \
-    # libgstapp-1.0
+    curl
+
 mkdir ~/OV.Work 
 cd ~/OV.Work 
+
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py 
 sudo -H python3.7 get-pip.py 
 
@@ -43,30 +43,31 @@ sudo tar xf l_openvino_toolkit_runtime_raspbian_p*.tgz --strip 1 -C ${INSTALL_DI
 #
 # Replace OpenCV
 #
-sudo rm -r -f ${INSTALL_DIR}/opencv
-
 curl https://packages.microsoft.com/config/debian/stretch/multiarch/prod.list > ./microsoft-prod.list
 sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && \
 sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo apt-get update
-sudo apt-get install -y moby-engine moby-cli
+sudo apt-get install -y moby-engine
+sudo apt-get install -y moby-cli
+rm ./microsoft* && \
 sudo docker run --name opencv daisukeiot/openvino-container:raspbian_opencv_data /bin/bash 
 sudo docker cp opencv:/data/opencv3.7.tar.gz ./
-sudo mkdir ${INSTALL_DIR}
 sudo tar -xf opencv3.7.tar.gz -C ${INSTALL_DIR} --strip-components 1
+
 #
 # Clean up docker
 #
 sudo docker system prune -a -f
 sudo apt-get remove -y --purge moby-cli
 sudo apt-get remove -y --purge moby-engine
-export PYTHONPATH=/opt/intel/openvino/opencv3.7/lib/python2.7/dist-packages/:$PYTHONPATH
-export PYTHONPATH=/opt/intel/openvino/opencv3.7/lib/python3.7/dist-packages/:$PYTHONPATH
-export LD_LIBRARY_PATH=/opt/intel/openvino/opencv3.7/lib/:$LD_LIBRARY_PATH
 
 echo "source $INSTALL_DIR/bin/setupvars.sh -pyver 3.7" >> ${HOME}/.bashrc 
 source ${INSTALL_DIR}/bin/setupvars.sh 
+export PYTHONPATH=${INSTALL_DIR}/python3.7/lib/python2.7/dist-packages/:$PYTHONPATH
+export PYTHONPATH=${INSTALL_DIR}/python3.7/lib/python3.7/dist-packages/:$PYTHONPATH
+export LD_LIBRARY_PATH=${INSTALL_DIR}/python3.7/lib/:$LD_LIBRARY_PATH
+
 sudo usermod -a -G users "$(whoami)" 
 sh /opt/intel/openvino/install_dependencies/install_NCS_udev_rules.sh 
 rm -r -f ~/OV.Work 
