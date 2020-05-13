66331531b29a938277814b1694c09502a18c19f4
Daisuke Nakahara <daisuken@msn.com>
2020-05-12 19:02:36 -0700

    Update for Raspbian

--- App/ObjectDetection/Python/setup.sh
+++ App/ObjectDetection/Python/setup.sh
@@ -1,7 +1,18 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 if [ ! -d "open_model_zoo" ]; then
-  git clone https://github.com/opencv/open_model_zoo.git
+  if [ -f /etc/lsb-release ]; then
+    # Ubuntu
+    git clone https://github.com/opencv/open_model_zoo.git
+  elif [ -f /usr/bin/lsb_release ]; then
+    # Raspbian?
+    model=$(cat /proc/cpuinfo | grep  "Model" | cut -d ":" -f2 | cut -d " " -f2)
+    if [ "$model" = "Raspberry" ]; then
+      wget --no-check-certificate https://github.com/opencv/open_model_zoo/archive/2019_R3.1.tar.gz -O open_model_zoo.tar.gz
+      mkdir open_model_zoo && tar -xf open_model_zoo.tar.gz -C open_model_zoo --strip-components 1
+      rm open_model_zoo.tar.gz
+    fi
+  fi
 fi
 
 if [ -d ${INTEL_OPENVINO_DIR}/deployment_tools/model_optimizer ]; then
