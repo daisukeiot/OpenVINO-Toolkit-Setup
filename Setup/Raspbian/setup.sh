477536e11de81e81c74c840de7869eabe6c9b926
Daisuke Nakahara <daisuken@msn.com>
2020-05-13 11:08:31 -0700

    Update setup.sh

--- Setup/Raspbian/setup.sh
+++ Setup/Raspbian/setup.sh
@@ -14,25 +14,24 @@ sudo apt-get install -y \
     cmake \
     unzip \
     pkg-config \
-    libjpeg-dev \
-    libpng-dev \
-    libtiff-dev \
-    libpng12-dev \
-    libavcodec-dev \
-    libavformat-dev \
-    libswscale-dev \
-    libv4l-dev \
-    libxvidcore-dev \
-    libx264-dev \
-    libgtk-3-dev \
-    libcanberra-gtk* \
-    libatlas-base-dev \
     gfortran \
-    python3-pip \
     curl
+
+# sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev
+# sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+# sudo apt-get install -y libxvidcore-dev libx264-dev
+# sudo apt-get install -y libgtk-3-dev
+# sudo apt-get install -y libcanberra-gtk*
+# sudo apt-get install -y libatlas-base-dev gfortran
+# sudo apt-get install -y gstreamer1.0-tools gstreamer1.0-alsa
+# sudo apt-get install -y python3-dev
+#libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools
 
 mkdir ~/OV.Work 
 cd ~/OV.Work 
+
+sudo apt-get install -y \
+    python3-pip
 
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py 
 sudo -H python3.7 get-pip.py 
@@ -64,9 +63,9 @@ sudo apt-get remove -y --purge moby-engine
 
 echo "source $INSTALL_DIR/bin/setupvars.sh -pyver 3.7" >> ${HOME}/.bashrc 
 source ${INSTALL_DIR}/bin/setupvars.sh 
-export PYTHONPATH=${INSTALL_DIR}/python3.7/lib/python2.7/dist-packages/:$PYTHONPATH
-export PYTHONPATH=${INSTALL_DIR}/python3.7/lib/python3.7/dist-packages/:$PYTHONPATH
-export LD_LIBRARY_PATH=${INSTALL_DIR}/python3.7/lib/:$LD_LIBRARY_PATH
+export PYTHONPATH=${INSTALL_DIR}/opencv3.7/lib/python2.7/dist-packages/:$PYTHONPATH
+export PYTHONPATH=${INSTALL_DIR}/opencv3.7/lib/python3.7/dist-packages/:$PYTHONPATH
+export LD_LIBRARY_PATH=${INSTALL_DIR}/opencv3.7/lib/:$LD_LIBRARY_PATH
 
 sudo usermod -a -G users "$(whoami)" 
 sh /opt/intel/openvino/install_dependencies/install_NCS_udev_rules.sh 
