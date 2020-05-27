9eae505d4d9c93cf53063dfe0583095587051008
Daisuke Nakahara <daisuken@msn.com>
2020-05-26 17:32:27 -0700

    Update build-OpenVINO.sh

--- Container/Raspbian/build-OpenVINO.sh
+++ Container/Raspbian/build-OpenVINO.sh
@@ -1,1 +1,16 @@
-docker image build -t openvino_armhf OpenVINO-crosscompile
+if [ $# -ne 2 ]
+  then
+    echo "======================================="
+    echo "Please specify Ubuntu Version and reistry"
+    echo "  Tag              : Tag for the image"
+    echo "  Registry         : Your registry"
+    echo "  Example ./build-OpenVINO-Toolkit.sh openvino myregistry"
+    echo "======================================="
+    exit
+fi
+
+IMAGE_TAG=$1
+MY_REGISTRY=$2
+TAG=${MY_REGISTRY}/openvino-container:${IMAGE_TAG}
+
+docker image build -t ${TAG} OpenVINO-crosscompile
